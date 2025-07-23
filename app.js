838c59eefcef4f5705bb491cd16220441088057f
Davis Kamau <daviskamau810@gmail.com>
2025-07-23 17:56:50 +0300

    for loop

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-let counter = 1
+/* 
+write a for loop that loops from 1 to 15
+if the number is odd print "ASAP"
+if the number is even print "Frontend"
+if the number is divisible by 5 print "ASAP frontend"
+*/
+for (let i = 1; i <= 15; i++){
+    if (i % 5 ===0)
+        console.log(`${i} - ASAP frontend`);
+    else if (i % 2 === 0)
+        console.log(`${i} - Frontend`);
+    else
+        console.log(`${i} - ASAP`);
 
-while(counter <= 20 ){
-    console.log(counter);
-    counter=counter + 1;
     
-}
-console.log("while loop finished");
+    }
