1d3f9b5304c142eeedf72911dcf05d2070131dd3
Davis Kamau <daviskamau810@gmail.com>
2025-07-24 12:53:17 +0300

    quick exercise

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-/* 
-write a for loop that loops from 1 to 15
-if the number is odd print "ASAP"
-if the number is even print "Frontend"
-if the number is divisible by 5 print "ASAP frontend"
-*/
-for (let i = 1; i <= 15; i++){
-    if (i % 5 ===0)
-        console.log(`${i} - ASAP frontend`);
-    else if (i % 2 === 0)
-        console.log(`${i} - Frontend`);
-    else
-        console.log(`${i} - ASAP`);
-
-    
-    }
+/*print out all the characters from the string "ASAP frontend"*/
+let str = "ASAP frontend";
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
