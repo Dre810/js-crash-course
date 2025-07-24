1d07f39814b2dbf7bf3efcb26d6adac7d3462836
Davis Kamau <daviskamau810@gmail.com>
2025-07-24 15:21:25 +0300

    arrays

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-/*print out all the characters from the string "ASAP frontend"*/
-let str = "ASAP frontend";
-for (let i = 0; i < str.length; i++) {
-    console.log(str[i]);
-}
+/*filter out the adults only*/
+let people = [18, 20, 16, 15, 21]
+
+let adults = people.filter((element) => element >= 18);
+    
+    
+
+console.log(adults);
