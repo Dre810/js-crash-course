ebb72580c21c873545236b7fe6d57dd8ecce100c
Davis Kamau <daviskamau810@gmail.com>
2025-07-25 18:19:15 +0300

    objects

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-/*filter out the adults only*/
-let people = [18, 20, 16, 15, 21]
 
-let adults = people.filter((element) => element >= 18);
-    
-    
+let users = [{
+    email: "example@gmail.com",
+    password: "password123",
+    name: "John Doe",
+    discord: "example#1234",
+    subscription: "VIP+",
+    lessonCompleted: [1, 2, 3]
+},
 
-console.log(adults);
+{
+    email: "davis@gmail.com",
+    password: "password123",
+    name: "Davis",
+    discord: "Daviscodes",
+    subscription: "VIP+",
+    lessonCompleted: [1, 2, 3, 4, 5]
+}
+];
+
+console.log(users[1].name);
+
+
